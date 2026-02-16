20497a8119d20c27bc7731945f597125f3575a43
Riteek Doble <pawarriteek78@gmail.com>
2026-02-16 13:02:02 +0530

    new data added

--- src/main/java/com/project/SpringFristProject/SpringFristProjectApplication.java
+++ src/main/java/com/project/SpringFristProject/SpringFristProjectApplication.java
@@ -1,13 +1,24 @@
 package com.project.SpringFristProject;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-public class SpringFristProjectApplication {
+public class SpringFristProjectApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringFristProjectApplication.class, args);
+
+	}
+	@Override
+	public void run(String... args) throws Exception {
+		Student student = new Student();
+		student.setId(29);
+		student.setName("Riteek");
+		System.out.println(student.getId());
+		System.out.println(student.getName());
 	}
 
 }
--- src/main/java/com/project/SpringFristProject/Student.java
+++ src/main/java/com/project/SpringFristProject/Student.java
@@ -1,4 +1,31 @@
 package com.project.SpringFristProject;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Student {
+    private int id;
+    private String name;
+
+    // 2. No-argument constructor
+    public Student() {
+    }
+
+    // 3. Getter and Setter methods
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
+
